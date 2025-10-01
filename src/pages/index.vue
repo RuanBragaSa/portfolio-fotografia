ece4ee75679064bb5ddc381eda92a8fd75b04121
Macbook Pro de Diego <tauildiego@icloud.com>
2025-09-30 22:18:48 -0300

    feat: removing layouts and finishing app-bar

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,9 +1,66 @@
 <template>
-  <div>
-    <v-icon icon="material-symbols--camera"></v-icon>
-  </div>
+  <v-layout>
+    <v-app-bar id="app-bar" :color="appBarColor" :elevation="0" style="position: fixed;" >
+      <v-app-bar-title class="title" style="color: rgb(var(--v-theme-silver));"><b>Roberta</b></v-app-bar-title>
+
+      <template v-slot:append>
+        <a href="#sobre">Sobre</a>
+        <a href="#portfolio">Portfólio</a>
+        <a href="#contato">Contato</a>
+      </template>
+    </v-app-bar>
+
+    <v-main>
+      <div style="height: 100vh; background-color: red;">
+        <section id="sobre">
+          <h2>Sobre</h2>
+          <p>Conteúdo da seção Sobre...</p>
+        </section>
+      </div>
+      <div style="height: 100vh; background-color: blue;">
+        <section id="portfolio">
+          <h2>Portfólio</h2>
+          <p>Conteúdo da seção Portfólio...</p>
+        </section>
+      </div>
+      <div style="height: 100vh; background-color: green;">
+        <section id="contato">
+          <h2>Contato</h2>
+          <p>Conteúdo da seção Contato...</p>
+        </section>
+      </div>
+    </v-main>
+
+
+  </v-layout>
 </template>
 
-<script>
-  //
+<script setup>
+
+const appBarColor = ref("transparent")
+
+onMounted(() => {
+  window.addEventListener("scroll", () => {
+    appBarColor.value = window.scrollY === 0 ? "transparent" : "rgb(var(--v-theme-charcoal))"
+  })
+})
 </script>
+
+<style>
+  @import url('https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400..900;1,400..900&display=swap');
+  @import url('https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap');
+
+  .title {
+    font-family: 'Playfair Display', serif;
+  }
+
+  .text {
+    font-family: "Inter", sans-serif;
+  }
+
+  #app-bar {
+    transition: all 0.3s 10ms;
+    transition-timing-function: ease;
+    background-repeat: no-repeat;
+  }
+</style>
--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -5,54 +5,52 @@
  */
 
 // Styles
-import '@mdi/font/css/materialdesignicons.css'
-import 'vuetify/styles'
+import "@mdi/font/css/materialdesignicons.css";
+import "vuetify/styles";
 
 // Composables
-import { createVuetify } from 'vuetify'
+import { createVuetify } from "vuetify";
+
+const defaultTheme = {
+  dark: true,
+  colors: {
+    background: "hsl(0 0% 6% )",
+    foreground: "hsl(0 0% 95%)",
+    silver: "hsl(0 0% 75%)",
+    platinum: "hsl(220 10% 85%)",
+    graphite: "hsl(220 8% 25%)",
+    charcoal: "hsl(220 10% 15%)",
+  },
+  variables: {
+    "container-max-width": "1400px",
+    "border-radius": "0.75rem",
+    "shadow-elegant": "0 25px 50px -12px rgba(0,0,0,0.25)",
+    "shadow-glow": "0 0 40px rgba(0,0,0,0.15)",
+    "blur-glass": "20px",
+    "padding-default": "2rem",
+  },
+  defaults: {
+    VContainer: {
+      maxWidth: "1400px",
+      class: "pa-8", // 2rem ~ pa-8 no Vuetify
+    },
+    VCard: {
+      rounded: "lg", // mapeia p/ 0.75rem se ajustar no theme
+      elevation: 4,
+    },
+  },
+};
 
 // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
 export default createVuetify({
   icons: {
-    defaultSet: "mdi"
+    defaultSet: "mdi",
   },
   theme: {
-    defaultTheme: 'system',
-    themes: [
-      {
-        dark: false,
-        colors: {
-          background: 'hsl(0 0% 6% )',
-          foreground: 'hsl(0 0% 95%)',
-          silver: 'hsl(0 0% 75%)',
-          platinum: 'hsl(220 10% 85%)',
-          graphite: 'hsl(220 8% 25%)',
-          charcoal: 'hsl(220 10% 15%)',
-        },
-        variables: {
-          'container-max-width': '1400px',
-          'border-radius': '0.75rem',
-          'shadow-elegant': '0 25px 50px -12px rgba(0,0,0,0.25)',
-          'shadow-glow': '0 0 40px rgba(0,0,0,0.15)',
-          'blur-glass': '20px',
-          'padding-default': '2rem'
-        },
-        defaults: {
-          VContainer: {
-            maxWidth: '1400px',
-            class: 'pa-8', // 2rem ~ pa-8 no Vuetify
-          },
-          VCard: {
-            rounded: 'lg', // mapeia p/ 0.75rem se ajustar no theme
-            elevation: 4,
-          },
-        },
-      },
-      
-    ]
+    defaultTheme: "defaultTheme",
+    themes: {defaultTheme},
   },
-})
-
+});
 
 // 'gradient-primary': 'linear-gradient(135deg, hsl(0 0% 6%) 0%, hsl(220 10% 15%) 50%, hsl(0 0% 8%) 100%)',
 // 'gradient-metalic': 'linear-gradient(135deg, hsl(220 10% 85%) 0%, hsl(0 0% 75%) 50%, hsl(220 10% 85%) 100%)' ,
